f1deb99b134d2cfbfcb8cd8edfef7870c0221faa
Pongsatorn Chanpanichravee <pongsatorn1612@berkeley.edu>
2024-05-07 00:19:10 -0700

    Start ediit expose

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,7 +1,13 @@
 // expose.js
-
+// document selectors, event listeners, and canvas.
+// document gaph it (e.x) let myNNavbar =  document.querySelector('#navbar')
+// document.getElementById(“id-of-your-element”)
+// document.getElementsByClassName(“class-of-your-elements”)
+// document.querySelector("css-selector")
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const hornSelect = document.getElementById("horn-select");
+  const hornIma = document.getElementById("horn-select");
+
 }
